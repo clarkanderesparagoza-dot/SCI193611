8679269eedde27e537ca948aec25aeb0fb0698f8
clarkanderesparagoza-dot <clarkanderesparagoza@gmail.com>
2026-07-12 19:22:00 +0700

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,14 @@
+# addition.py
+# -----------
+
+
+"""
+Run python autograder.py
+"""
+
+
+def add(a, b):
+    "Return the sum of a and b"
+    "*** YOUR CODE HERE ***"
+    sum = a + b
+    return sum
